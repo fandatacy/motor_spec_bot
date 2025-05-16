492e72858e3e2f9e8399118825a82e6d07983643
fandatacy <sagarghosh.sg1@gmail.com>
2025-05-16 15:51:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,61 +7,69 @@ BOT_TOKEN = "7885279501:AAFU7y-RsZ9S3OGNp1ekh1KIX1fBibbtchI"
 with open("data.json") as f:
     motor_data = json.load(f)
 
-def find_closest_kw(requested_kw):
-    requested_kw = float(requested_kw)
-    available_kws = [float(k) for k in motor_data.keys()]
-    closest = min(available_kws, key=lambda x: abs(x - requested_kw))
-    return str(closest)
+def normalize_kw(input_str):
+    try:
+        num = float(input_str)
+        if num.is_integer():
+            return str(int(num))
+        return "{0:.10f}".format(num).rstrip('0').rstrip('.')
+    except ValueError:
+        return None
+
+def format_motor_info(data, kw):
+    # Format MPCB recommendations
+    mpcb_recommend = "\nMPCB Recommendations:\n"
+    for brand in ['abb', 'siemens', 'lt', 'schneider']:
+        mpcb_recommend += f"- {brand.upper()}: `{data['mpcb'][brand]['recommend']}` ({data['mpcb'][brand]['range']})\n"
+
+    # Format Contactor recommendations
+    contactor_recommend = "\nContactor Recommendations:\n"
+    for brand in ['abb', 'siemens', 'lt']:
+        contactor_recommend += f"- {brand.upper()}: `{data['contactor'][brand]['recommend']}` ({data['contactor'][brand]['amp']}A)\n"
+
+    # Format MCCB data
+    mccb_info = "\nMCCB Ratings:\n"
+    for brand in ['abb', 'siemens', 'lt', 'schneider']:
+        mccb_info += f"- {brand.upper()}: `{data['mccb'][brand]}`\n"
+
+    return f'''
+Motor Details for {kw} kW
+
+Horsepower (HP): `{data["hp"]}`
+Full Load Current (Star): `{data["flc_star"]} A`
+Full Load Current (Delta): `{data["flc_delta"]} A`
+{mpcb_recommend}
+{contactor_recommend}
+{mccb_info}
+Cable Size:
+- Aluminium: `{data["cable"]["al"]}`
+- Copper: `{data["cable"]["cu"]}`
+Cable Gland: `{data["cable_gland"]}`
+
+Bearing Numbers:
+- ABB: DE `{data["bearing"]["abb_de"]}`, NDE `{data["bearing"]["abb_nde"]}`
+- Siemens: DE `{data["bearing"]["siemens_de"]}`, NDE `{data["bearing"]["siemens_nde"]}`
+'''
 
 async def motor_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not context.args:
-        await update.message.reply_text("Please provide motor kW like: `/motor 15`")
+        await update.message.reply_text("Please provide motor kW. Example: /motor 5.5")
         return
-    
-    try:
-        requested_kw = float(context.args[0])
-        kw_key = find_closest_kw(requested_kw)
-        data = motor_data.get(kw_key)
 
-        if not data:
-            await update.message.reply_text("Motor kW not found in database.")
-            return
+    kw_input = context.args[0]
+    normalized_kw = normalize_kw(kw_input)
 
-        reply = f"""<b>Motor Details for {kw_key} kW</b>\n
-<b>Horsepower (HP):</b> {data['hp']} HP
-<b>Full Load Current:</b>
-- <b>Star:</b> {data['flc_star']} A
-- <b>Delta:</b> {data['flc_delta']} A
+    if normalized_kw is None:
+        await update.message.reply_text("Invalid input. Please use a number (e.g., /motor 5.5)")
+        return
 
-<b>MPCB Rating:</b>
-- ABB: {data['mpcb']['abb']} ({data['mpcb']['abb_range']})
-- Siemens: {data['mpcb']['siemens']} ({data['mpcb']['siemens_range']})
-- L&T: {data['mpcb']['lt']} ({data['mpcb']['lt_range']})
-- Schneider: {data['mpcb']['schneider']} ({data['mpcb']['schneider_range']})
+    data = motor_data.get(normalized_kw)
+    if data:
+        reply = format_motor_info(data, normalized_kw)
+        await update.message.reply_text(reply, parse_mode="Markdown")
+    else:
+        await update.message.reply_text(f"Data not found for {normalized_kw} kW. Available sizes: {', '.join(sorted(motor_data.keys()))}")
 
-<b>Contactor:</b>
-- ABB: {data['contactor']['abb']} ({data['contactor']['abb_amp']} A)
-- Siemens: {data['contactor']['siemens']} ({data['contactor']['siemens_amp']} A)
-- L&T: {data['contactor']['lt']} ({data['contactor']['lt_amp']} A)
-
-<b>MCCB:</b> {data['mccb']}
-
-<b>Cable Size:</b>
-- Aluminium: {data['cable']['al']}
-- Copper: {data['cable']['cu']}
-
-<b>Cable Gland:</b> {data['cable_gland']}
-
-<b>Bearings:</b>
-- ABB: DE: {data['bearing']['abb_de']} | NDE: {data['bearing']['abb_nde']}
-- Siemens: DE: {data['bearing']['siemens_de']} | NDE: {data['bearing']['siemens_nde']}
-"""
-        await update.message.reply_text(reply, parse_mode="HTML")
-
-    except ValueError:
-        await update.message.reply_text("Please provide a valid number like: `/motor 7.5`")
-
-# Setup Application
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("motor", motor_info))
 app.run_polling()
